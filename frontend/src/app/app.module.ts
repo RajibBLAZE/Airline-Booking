89c7028d877b189df44176831b68670267b57aa8
Blaze Automation <rajib@blazeautomation.com>
2025-05-18 00:54:28 +0530

    Updated Backend part partially completed

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { VerifyEmailComponent } from './auth/verify-email/verify-email.component';
-import { LoginComponent } from './auth/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SignupComponent,
-    VerifyEmailComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
